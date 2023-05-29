c3f64b4c8be4c3279b81db91cae3d0eca30c3058
Mila-Golyshkina <129863279+Mila-Golyshkina@users.noreply.github.com>
2023-05-29 17:16:43 +0400

    Update index.js

--- index.js
+++ index.js
@@ -15,10 +15,37 @@ const content = fs.readFileSync(path.join(
 ), 'utf-8');
 
 // BEGIN
-const data = content.split("\r\n").slice(1);
-const modifiedData = data.map((element) => {
-  const modifiedElement = element.split("|").slice(1, 8).join(", ");
-  return modifiedElement;
-});
-console.log(`количество существ в таблице ${modifiedData.length}`);
+
+// console.log(content)
+let data = content.split("\r\n").slice(1);
+const creaturesNum = data.length;
+console.log(`1) Видов существ в таблице:`, creaturesNum);
+// console.log(data);
+
+let power = data.map((row) => Number(row.split("|").at(2)));
+// console.log(power);
+let powerMax = Math.max(...power);
+// console.log(powerMax);
+let powerMaxIndex = power.indexOf(powerMax);
+// console.log(powerMaxIndex);
+
+let price = data.map((row) => Number(row.split("|").at(7)));
+let pricePowerMaxIndex = price[powerMaxIndex];
+// console.log(pricePowerMaxIndex);
+let quantityPM = 10;
+
+let arrTwoPowerMax = power.slice(0);
+arrTwoPowerMax[powerMaxIndex] = -Infinity;
+// console.log(arrTwoPowerMax);
+let powerTwoMax = Math.max(...arrTwoPowerMax);
+// console.log(powerTwoMax);
+let powerTwoMaxIndex = power.indexOf(powerTwoMax);
+let priceTwoPowerMaxIndex = price[powerTwoMaxIndex];
+let quantityTwoPM = 20;
+
+console.log(
+  `2) 10 самых сильных существ и 20 вторых по силе стоят:`,
+  quantityPM * pricePowerMaxIndex + quantityTwoPM * priceTwoPowerMaxIndex
+);
+
 // END
