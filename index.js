383539c10d44edd21fa559ff552bc4984d89242f
Mila-Golyshkina <129863279+Mila-Golyshkina@users.noreply.github.com>
2023-05-30 07:48:22 +0400

    Update index.js

--- index.js
+++ index.js
@@ -16,36 +16,65 @@ const content = fs.readFileSync(path.join(
 
 // BEGIN
 
-// console.log(content)
-let data = content.split("\r\n").slice(1);
-const creaturesNum = data.length;
-console.log(`1) Видов существ в таблице:`, creaturesNum);
-// console.log(data);
+// Step 1
+const rows = content.split('\r\n');
+const data = rows.slice(1);
 
-let power = data.map((row) => Number(row.split("|").at(2)));
-// console.log(power);
-let powerMax = Math.max(...power);
-// console.log(powerMax);
-let powerMaxIndex = power.indexOf(powerMax);
-// console.log(powerMaxIndex);
+const creatures = data.map((creature) => creature.split('|')[1]);
+console.log(`Количество видов существ: ${creatures.length}`);
 
-let price = data.map((row) => Number(row.split("|").at(7)));
-let pricePowerMaxIndex = price[powerMaxIndex];
-// console.log(pricePowerMaxIndex);
-let quantityPM = 10;
-
-let arrTwoPowerMax = power.slice(0);
-arrTwoPowerMax[powerMaxIndex] = -Infinity;
-// console.log(arrTwoPowerMax);
-let powerTwoMax = Math.max(...arrTwoPowerMax);
-// console.log(powerTwoMax);
-let powerTwoMaxIndex = power.indexOf(powerTwoMax);
-let priceTwoPowerMaxIndex = price[powerTwoMaxIndex];
-let quantityTwoPM = 20;
-
-console.log(
-  `2) 10 самых сильных существ и 20 вторых по силе стоят:`,
-  quantityPM * pricePowerMaxIndex + quantityTwoPM * priceTwoPowerMaxIndex
+// Data Normalization
+const collection = data.map((row) => row.split('|'));
+const normalizedData = collection.map((item) =>
+  item.filter((elem) => elem).map((elem) => elem.trim())
 );
 
+// Step 2
+const strengthOrder = _.sortBy(normalizedData, (creature) =>
+  Number(creature[1])
+);
+const strongestUnit = strengthOrder[strengthOrder.length - 1];
+const secondStrongestUnit = strengthOrder[strengthOrder.length - 2];
+const tenStrongest = strongestUnit[6] * 10;
+const twentySecondStrongest = secondStrongestUnit[6] * 20;
+
+console.log(`Стоимость найма 10 самых сильных существ: ${tenStrongest}
+Стоимость найма 20 вторых по силе существ: ${twentySecondStrongest}`);
+
+// Step 3
+const maxWeightCreature = _.maxBy(normalizedData, (creature) =>
+  Number(creature[5])
+);
+
+const minWeightCreature = _.minBy(normalizedData, (creature) =>
+  Number(creature[5])
+);
+
+const priceSquad = Number(maxWeightCreature[6]) + Number(minWeightCreature[6]);
+
+console.log(`Стоимость отряда самых толстых и худых: ${priceSquad}`);
+
+// Step 4
+const strengthAndPrice = _.sortBy(normalizedData, (creature) => {
+  return Number(creature[1]) / Number(creature[6]);
+});
+
+const mostProfitableUnit = strengthAndPrice[strengthAndPrice.length - 1];
+const leastProfitableUnit = strengthAndPrice[0];
+
+console.log(`Самый выгодный юнит по соотношению цены и силы: ${mostProfitableUnit[0]}
+Самый невыгодный юнит по соотношению цены и силы: ${leastProfitableUnit[0]}`);
+
+// Step 5
+const money = 10000;
+const strongestOrder = _.sortBy(
+  normalizedData,
+  (creature) => (money / Number(creature[6])) * creature[1]
+);
+
+const strongestArmy = strongestOrder[strongestOrder.length - 1];
+const unitsCount = money / strongestArmy[6];
+console.log(
+  `Самый сильная армия за 10000: ${unitsCount} ${strongestArmy[0]}ов`
+);
 // END
